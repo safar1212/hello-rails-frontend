d58a24a96699729e677eabfd158ead0c9bc798cc
safar ali <miqra019@gmail.com>
2022-10-22 10:31:13 +0500

    Create app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Greeting from './components/Greeting';
+import Home from './components/Home';
+import NaVBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Hello Reacto</h1>
+        <NaVBar />
+        <Routes>
+          <Route exact path="/*" element={<Home />} />
+          <Route exact path="/greet" element={<Greeting />} />
+        </Routes>
       </header>
     </div>
-  );
-}
+  )};
 
-export default App;
+  export default App;
